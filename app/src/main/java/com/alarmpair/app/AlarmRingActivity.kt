8eb1d6af770f3247af0c2b9eb2f04ee3a46d1704
Spyde-ally <dikshantgupta96@gmail.com>
2026-09-19 01:21:38 +0530

    Add import for Intent in AlarmRingActivity

--- app/src/main/java/com/alarmpair/app/AlarmRingActivity.kt
+++ app/src/main/java/com/alarmpair/app/AlarmRingActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import android.content.Intent
 class AlarmRingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
